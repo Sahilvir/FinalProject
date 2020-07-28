5b1e4a66419669cf79a332ae37ebc3196cb06eb2
Sahilvir <dhil0044@algonquinlive.com>
2020-07-28 16:48:06 -0400

    geo

--- app/src/main/java/com/example/finalproject/GeoMain.java
+++ app/src/main/java/com/example/finalproject/GeoMain.java
@@ -1,4 +1,16 @@
 package com.example.finalproject;
 
-public class GeoMain {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GeoMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.geo_list_layout);
+    }
+
+
+
 }
