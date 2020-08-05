6d7a7190c8bff85ae154086d809204a733c42ebb
Jason <chow0126@algonquinlive.com>
2020-08-05 15:53:48 -0400

    All files for Deezer Song Search API Includes edits to MainActivity to reach the Deezer activity Also includes edit to AndroidManifest.xml to give internet permission, edit to colors.xml to add a colour, edit to build.gradle to allow use of Snackbars and language support for English and American English.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button deezerButton = (Button) findViewById(R.id.deezerSongSearchButton);
+        deezerButton.setOnClickListener( v -> {
+            Intent goToDeezer = new Intent(MainActivity.this, DeezerSearchMain.class);
+            startActivity(goToDeezer);
+        });
+
+
     }
 }
